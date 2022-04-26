bf431d83e612fbb662bff6db89b2204360650b4b
minjongkim <rlaalswhd200@naver.com>
2022-04-26 17:37:30 +0900

    Initial Commit

---
+++ Soyou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soyou
+//
+//  Created by 김민종 on 2022/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
