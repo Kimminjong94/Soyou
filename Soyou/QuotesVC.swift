15d7785900820acc42607cc61c3dfebbefbd2857
minjongkim <rlaalswhd200@naver.com>
2022-04-28 20:51:07 +0900

    adding storyboard views

---
+++ Soyou/QuotesVC.swift
@@ -0,0 +1,20 @@
+//
+//  QuotesVC.swift
+//  Soyou
+//
+//  Created by 김민종 on 2022/04/28.
+//
+
+import UIKit
+
+class QuotesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
