747d993e82371aa8c0912da2540192f108331052
minjongkim <rlaalswhd200@naver.com>
2022-05-13 13:36:21 +0900

    fixing api response

---
+++ Soyou/Emotion/EmotionResponse.swift
@@ -0,0 +1,22 @@
+//
+//  EmotionResponse.swift
+//  Soyou
+//
+//  Created by 김민종 on 2022/05/13.
+//
+
+import Foundation
+
+
+struct EmotionResponse: Decodable {
+    let total: Int
+    let results: [EmotionPhotoUrl]
+}
+
+struct EmotionPhotoUrl: Decodable {
+    let id: String
+    let urls: PhotoURLS
+}
+struct PhotoURLS: Decodable {
+    let raw: String
+}
