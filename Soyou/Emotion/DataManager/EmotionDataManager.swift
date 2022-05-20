48d77df45be6a743c8c209530969cacd5debdb05
minjongkim <rlaalswhd200@naver.com>
2022-05-20 21:59:03 +0900

    fixing alamofire

--- Soyou/Emotion/DataManager/EmotionDataManager.swift
+++ Soyou/Emotion/DataManager/EmotionDataManager.swift
@@ -7,32 +7,27 @@
 
 import Foundation
 
-//
-//class EmotionDataManager: delegate {
-//    
-//    func getQuotesTagPhoto(tagName: String, delegate: PhotoDelegate) {
-//        let group = DispatchGroup()
-//        group.enter()
-//        
-//        DispatchQueue.global(qos: .userInitiated).async {
-//            AF.request("https://api.unsplash.com/search/photos?page=1&query=\(tagName)&client_id=9xBbg0WlJNPczWsPplzB38AOIIBLXt6ugKaX6kzbj0o",
-//                       method: .get,
-//                       headers: nil)
-//            .validate()
-//            .responseDecodable(of: PhotoResponse.self) { response in
-//                switch response.result {
-//                case .success(let response):
-//                    print(response)
-//                    delegate.didSuccessGetPhoto(response.results[0].urls)
-//    //                delegate.didSuccessGetQuotes(response)
-//                case .failure:
-//                    print(response)
-//                    
-//                }
-//            }
-//        }
-//
-//    }
-//}
+
+class EmotionDataManager: EmotionDataManagerDelegate {
+    
+    func getQuotesTagPhoto(tagName: String, delegate: PhotoDelegate) {
+        
+            AF.request("https://api.unsplash.com/search/photos?page=1&query=\(tagName)&client_id=9xBbg0WlJNPczWsPplzB38AOIIBLXt6ugKaX6kzbj0o",
+                       method: .get,
+                       headers: nil)
+            .validate()
+            .responseDecodable(of: PhotoResponse.self) { response in
+                switch response.result {
+                case .success(let response):
+                    print(response)
+                    delegate.didSuccessGetPhoto(response.results[0].urls)
+    //                delegate.didSuccessGetQuotes(response)
+                case .failure:
+                    print(response)
+            }
+        }
+
+    }
+}
 
 
