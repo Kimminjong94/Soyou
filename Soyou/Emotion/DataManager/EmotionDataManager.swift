f40be2900bb752d05ee7e97792b5ad1afc705e67
kimminjong <easyletgothen@gmail.com>
2022-07-28 14:56:07 +0900

    에러 수정

--- Soyou/Emotion/DataManager/EmotionDataManager.swift
+++ Soyou/Emotion/DataManager/EmotionDataManager.swift
@@ -1,35 +1,35 @@
+////
+////  EmotionDataManager.swift
+////  Soyou
+////
+////  Created by 김민종 on 2022/05/12.
+////
 //
-//  EmotionDataManager.swift
-//  Soyou
+//import Foundation
+//import Alamofire
 //
-//  Created by 김민종 on 2022/05/12.
 //
-
-import Foundation
-import Alamofire
-
-
-class EmotionDataManager: EmotionDataManagerDelegate {
-    func getEmotionData(delegate: EmotionDelegate) {
-        AF.request("https://api.unsplash.com/search/photos?page=1&query=\(tagName)&client_id=9xBbg0WlJNPczWsPplzB38AOIIBLXt6ugKaX6kzbj0o",
-                   method: .get,
-                   headers: nil)
-        .validate()
-        .responseDecodable(of: PhotoResponse.self) { response in
-            switch response.result {
-            case .success(let response):
-                print(response)
-                delegate.didSuccessGetPhoto(response.results[0].urls)
-//                delegate.didSuccessGetQuotes(response)
-            case .failure:
-                print(response)
-        }
-    }
-    }
-    
-    func getEmotionTagPhoto(tagName: String, delegate: EmotionDelegate) {
-        
-    }
-}
-
-
+//class EmotionDataManager: EmotionDataManagerDelegate {
+//    func getEmotionData(delegate: EmotionDelegate) {
+//        AF.request("https://api.unsplash.com/search/photos?page=1&query=\(tagName)&client_id=9xBbg0WlJNPczWsPplzB38AOIIBLXt6ugKaX6kzbj0o",
+//                   method: .get,
+//                   headers: nil)
+//        .validate()
+//        .responseDecodable(of: PhotoResponse.self) { response in
+//            switch response.result {
+//            case .success(let response):
+//                print(response)
+//                delegate.didSuccessGetPhoto(response.results[0].urls)
+////                delegate.didSuccessGetQuotes(response)
+//            case .failure:
+//                print(response)
+//        }
+//    }
+//    }
+//    
+//    func getEmotionTagPhoto(tagName: String, delegate: EmotionDelegate) {
+//        
+//    }
+//}
+//
+//
